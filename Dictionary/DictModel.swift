bd5fe76e123bb253dd87cd802861c10cd8ddae81
anirakanna <evgenia.shipova@gmail.com>
2020-05-05 20:32:35 +0300

    create model

---
+++ Dictionary/DictModel.swift
@@ -0,0 +1,13 @@
+//
+//  DictModel.swift
+//  Dictionary
+//
+//  Created by Evgenia Shipova on 05.05.2020.
+//  Copyright © 2020 Evgenia Shipova. All rights reserved.
+//
+
+import UIKit
+struct Dictionary {
+    let word: String
+    let translatedWord: String
+}
---
+++ Dictionary/DictionaryViewController.swift
@@ -0,0 +1,48 @@
+//
+//  DictionaryViewController.swift
+//  Dictionary
+//
+//  Created by Evgenia Shipova on 05.05.2020.
+//  Copyright © 2020 Evgenia Shipova. All rights reserved.
+//
+
+import UIKit
+
+class DictionaryViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    // MARK: - Table view data source
+
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return 1
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+
+        
+
+        return cell
+    }
+    
+
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
